594988895b696f5cd48a33fd1e4f6c376b4909be
Pavel Mostovoy <pavelm@bemobi.com>
2021-12-03 18:42:00 +0200

    examples added

---
+++ app.py
@@ -0,0 +1,14 @@
+var_1 = "volume1"
+var_2 = ["v","o","l","u","m","e","1"]
+
+var_3 = var_1[:-1] + "2"
+
+print(var_3)
+
+
+print(var_1.upper())
+
+var_2.append(345)
+v1 = var_2.pop()
+
+print(var_2, v1)
