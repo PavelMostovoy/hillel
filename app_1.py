81deff9fe63cce15a95d33325cbc1f45c13f1739
Pavel Mostovoy <pavelm@bemobi.com>
2021-12-06 19:16:23 +0200

    examples added

--- app_1.py
+++ app_1.py
@@ -1,11 +1,24 @@
-x = 1
+"""
+if x even /2
+if x odd * 3 +1
+"""
+print("In case of filling '0' programm will end")
 
 while True:
-    if 50 < x < 60:
-        continue
-    print("runing")
-    x = x + 1
-    if x == 70:
-        break
 
-    print(x)
+    x = int(input("Please input number : "))
+    if x == 0:
+        break
+    elif x < 0:
+        print("Should be positive only ")
+        x = abs(x)
+    i = 0
+    while x != 1:
+        i = i + 1
+        if x % 2 == 0:
+            x = x / 2
+        else:
+            x = (x * 3) + 1
+
+    print(f"Number of cycles {i}")
+
