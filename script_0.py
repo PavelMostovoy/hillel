4619ddd88aedfc6377a64e8d123f351784989978
Pavel Mostovoy <pavelm@bemobi.com>
2021-12-17 18:40:44 +0200

    examples added

--- script_0.py
+++ script_0.py
@@ -1,30 +1,30 @@
-# value_tuple = (0, 1, 2, 10, 3, 6, 12, 3, 1, 100, 12, 34, 3, "a", "b", "b")
-#
-# value_list = [0, 1, 2, 10, 3, 6, 12, 3, 1, value_tuple, 100, 12, 34, 3,
-# "a", "b", "b"]
-#
-# value_set = {0, 1, 2, 3, 3, value_tuple}
-#
-value_set_1 = (0, 1, 2, 10, 3, 6, 12, 3, 1, 100, 12, 34, 3, "a", "b", "b")
-#
-# value_set.add(value_list)
-#
-#
-# print(value_set)
-#
+from PlatesList import plates_list
 
-import random
+plates = list(set(plates_list))
 
-# x = int(random.randint(10, 100))
+auto = dict()
+
+marks = ["Ford", "Honda", "Mazda"]
+
+marks.append('Maseraty')
+
+su = len(plates)
+
+part = su // len(marks)
+
+# print(f'overall = {su}   part = {part}')
+
+set_1 = plates[:part]
+set_2 = plates[part:part*2]
+set_3 = plates[part*2: part*3]
+set_4 = plates[part*3:]
+data = [set_1, set_2, set_3, set_4]
 
 
-var = list()
+for i, mark in enumerate(marks):
+    # auto[mark] = data[i]
+    auto[mark] = plates[part*i: part*(i+1)]
 
-for i in range(10):
-    var.append(random.randint(10, 100))
 
-print(var)
 
-var_1 = [i*2 for i in value_set_1]
-
-print(var_1)
+print(auto)
