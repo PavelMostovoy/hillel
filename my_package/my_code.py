87177ff152232c9bb498dad77b94eb622de0555e
pavel mostovoy <pavel_mostovoy@msn.com>
2021-12-27 11:52:52 +0200

    modules added

---
+++ my_package/my_code.py
@@ -0,0 +1,13 @@
+
+def foo():
+    print("function")
+
+variable = ("inside var")
+
+
+class SomeClass:
+    def __init__(self):
+        self.var = "something inside class"
+
+    def prn(self):
+        print(self.var + "printed")
