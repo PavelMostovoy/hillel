e08c1a239280a703127929f464b7f32e192bf97e
pavel mostovoy <pavel_mostovoy@msn.com>
2022-01-31 19:25:29 +0200

    classes example

--- classes_example.py
+++ classes_example.py
@@ -1,31 +1,88 @@
+class PlateNumber:
+    colors = ["Red", "Green", "Blue"]
+    marks = ["Audi", "Toyota", "Bentley"]
 
-
-class PlateNumber():
     def __init__(self, region, number, extension, db):
-        self.number = number
-        self.region = region
-        self.extension = extension
-        self.db = db
+        self.__number = number
+        self.__region = region
+        self.__extension = extension
+        self.__db = db
+        self.__color = None
+        self.__year = int()
+        self.__mark = "Audi"
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, data):
+        self.__year = int(data)
+
+    def set_color(self, data):
+        if data in self.colors:
+            self.__color = data
+        else:
+            color = input("Incorrect color please fill correct :")
+            self.set_color(color)
+
+    def color(self):
+        return self.__color
+
+    @property
+    def mark(self):
+        return self.__mark
+
+    @mark.setter
+    def mark(self, data):
+        if data in self.marks:
+            self.__mark = data
+        else:
+            mark = input("Incorrect mark please fill correct :")
+            self.mark = mark
 
     def calculate_number(self):
-        return self.number
+        return self.__number
 
     def is_unique(self):
-        if self.region + self.number +self.extension in  self.db:
+        if self.__region + self.__number + self.__extension in self.db:
             return "exist"
         else:
             return "not in list"
 
 
-v1 = PlateNumber("aa", "0987", "nn", "fake")
+class Plates80(PlateNumber):
+    def __init__(self, region, number, extension, db):
+        super().__init__(region, number, extension, db)
+        self.__year = 1980
 
-print(v1.calculate_number())
+    @property
+    def year(self):
+        return self.__year
 
-class PlateWithOwher(PlateNumber):
+    @year.setter
+    def year(self, data):
+        if int(data) == 2017:
+            super().set_color(input("Enter color"))
+        if int(data) != 1980:
+            self.__year = 1980
 
-    def get_owner(self):
-        print("not implemented")
+    def set_color(self, data):
+        if data in self.colors:
+            self.__color = data
+        else:
+            self.__color = "Grey"
 
-v3 = PlateWithOwher("d","dddd","dd","ddd")
+    def color(self):
+        return self.__color
 
-v3.get_owner()
+
+a1 = Plates80("AA", "0000", "BB", "None")
+
+
+a1.set_color("Black")
+
+a1.year = 2017
+
+print(a1.color())
+
