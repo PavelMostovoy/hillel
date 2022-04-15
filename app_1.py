8a67ab6f01a6784afed05b5502e675933891a10b
Pavel Mostovoy <pavelm@bemobi.com>
2022-04-15 17:31:44 +0200

    Changes

--- app_1.py
+++ app_1.py
@@ -1,27 +1,41 @@
-"""
-if x even /2
-if x odd * 3 +1
-"""
-# print("In case of filling '0' programm will end")
 
-x = False
-while True:
+status_flag = True
 
-    x = int(input("Please input number :  " if x else
-                  "In case of filling '0'"
-                  " program will end \nPlease input number :  "))
-    if x == 0:
-        break
-    elif x < 0:
-        print("Should be positive only ")
-        x = abs(x)
-    i = 0
-    while x != 1:
-        i = i + 1
-        if x % 2 == 0:
-            x = x / 2
-        else:
-            x = (x * 3) + 1
+VARIABLE  = 1
+processed = "glob"
+data = {"initial":"data"}
 
-    print(f"Number of cycles {i}")
+def function_one(parameter):
+    # global VARIABLE
+    print("first func")
+    processed = f" something with {parameter}"
+    VARIABLE = 4
+    data = {"key":VARIABLE}
+    data["processed"] = processed
+
+    def internal(x):
+        nonlocal processed
+        print(x.upper()+ processed)
+
+    internal(processed)
+
+
+
+function_one("First")
+
+
+print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
